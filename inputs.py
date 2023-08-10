18fb8fa82a9f96ecd6de71944b4a9bc41cf4b73e
Kwaku Opoku-Ware <kwaku.opoku-ware@usu.edu>
2023-08-10 13:24:11 -0600

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -1,22 +1,31 @@
-# Inputs
+# Define grid dimensions
 nrow, ncol = 50, 50
 
-longitude = -89.4 * np.ones((nrow, ncol))  
+# Create 2D arrays for model inputs  
+
+# Longitude and latitude
+longitude = -89.4 * np.ones((nrow, ncol))
 latitude = 43.1 * np.ones((nrow, ncol))
 
+# Timestamp 
 datetime = np.full((nrow, ncol), datetime.datetime(2020, 6, 15, 12))
 
-Tair = 25 * np.ones((nrow, ncol))
+# Weather data 
+Tair = 25 * np.ones((nrow, ncol))  
 SWin = 500 * np.ones((nrow, ncol))
-u = 2 * np.ones((nrow, ncol)) 
+u = 2 * np.ones((nrow, ncol))
 ea = 2.5 * np.ones((nrow, ncol))
 pa = 101 * np.ones((nrow, ncol))
 
-LAI_grid = np.random.rand(nrow, ncol) * 6
+# Vegetation properties
+LAI_grid = np.random.rand(nrow, ncol) * 6  
 h_grid = np.random.rand(nrow, ncol) * 2
-T_grid = np.random.rand(nrow, ncol) * 10 + 285
 
+# Soil/canopy radiative properties
 albSoil = 0.25 * np.ones((nrow, ncol))
-albVeg = 0.2 * np.ones((nrow, ncol))  
+albVeg = 0.2 * np.ones((nrow, ncol))
 emissSoil = 0.95 * np.ones((nrow, ncol))
 emissVeg = 0.97 * np.ones((nrow, ncol))
+
+# Surface temperature grid 
+T_grid = np.random.rand(nrow, ncol) * 10 + 285
